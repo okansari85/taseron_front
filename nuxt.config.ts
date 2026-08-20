3fb5ca5bbf3f846c8151a088e066453196b3be45
OKAN SARI <sari_okan@hotmail.com>
2026-08-21 00:08:59 +0300

    fix: set local Laravel API base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBaseUrl: 'http://localhost:8000',
+      apiBaseUrl: 'http://127.0.0.1:8000',
     },
   },
   app: {
