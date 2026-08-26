5ed58bd3e48e55daf9d2a9bf5578dfaeb7fcedef
OKAN SARI <sari_okan@hotmail.com>
2026-08-27 00:48:29 +0300

    feat: connect users page to tenant sidebar

---
+++ app/pages/tenants/[tenantId]/users/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import UserManagementPanel from '~/components/users/UserManagementPanel.vue'
+</script>
+
+<template>
+  <div class="p-6">
+    <UserManagementPanel />
+  </div>
+</template>
