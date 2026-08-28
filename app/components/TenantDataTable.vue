28fa4a71e90983b7fc376f8fd836031b456a2325
OKAN SARI <sari_okan@hotmail.com>
2026-08-29 00:44:16 +0300

    fix tenant data table component resolution

---
+++ app/components/TenantDataTable.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import TenantsTenantDataTable from '~/components/tenants/TenantDataTable.vue'
+</script>
+
+<template>
+  <TenantsTenantDataTable v-bind="$attrs" />
+</template>
