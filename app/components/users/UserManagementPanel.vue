9c2137fc36a2da48b13576d8d8dc91a6b245bfaa
OKAN SARI <sari_okan@hotmail.com>
2026-09-04 01:41:28 +0300

    Remove duplicate user create button

--- app/components/users/UserManagementPanel.vue
+++ app/components/users/UserManagementPanel.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 import UserAuthorizationPanel from './UserAuthorizationPanel.vue'
-import UserCreateButton from './UserCreateButton.vue'
 </script>
 
 <template>
   <div class="space-y-3">
-    <div class="flex justify-end">
-      <UserCreateButton />
-    </div>
     <UserAuthorizationPanel />
   </div>
 </template>
