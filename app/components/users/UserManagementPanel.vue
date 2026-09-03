d8fa4c8a2a0ced3603bf83574ae6b282edbe2838
OKAN SARI <sari_okan@hotmail.com>
2026-09-03 16:02:18 +0300

    feat: restore new user button

--- app/components/users/UserManagementPanel.vue
+++ app/components/users/UserManagementPanel.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import UserAuthorizationPanel from './UserAuthorizationPanel.vue'
+import UserCreateButton from './UserCreateButton.vue'
 </script>
 
 <template>
-  <UserAuthorizationPanel />
+  <div class="space-y-3">
+    <div class="flex justify-end">
+      <UserCreateButton />
+    </div>
+    <UserAuthorizationPanel />
+  </div>
 </template>
