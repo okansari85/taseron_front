3ce66ded04e085c74aa4ec00578749d503331b68
OKAN SARI <sari_okan@hotmail.com>
2026-09-04 17:09:30 +0300

    show categorized users in authorization

--- app/components/users/UserManagementPanel.vue
+++ app/components/users/UserManagementPanel.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
-import UserAuthorizationPanel from './UserAuthorizationPanel.vue'
+import AuthorizationUsersPanel from './AuthorizationUsersPanel.vue'
 </script>
 
 <template>
   <div class="space-y-3 user-management-panel">
-    <UserAuthorizationPanel />
+    <AuthorizationUsersPanel />
   </div>
 </template>
 
 <style scoped>
-.user-management-panel :deep(thead th:last-child),
-.user-management-panel :deep(tbody td:last-child) {
-  display: none;
+.user-management-panel {
+  width: 100%;
 }
 </style>
