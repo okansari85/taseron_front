dd5be7be31042e682c241e367bc8b5c394f59ead
OKAN SARI <sari_okan@hotmail.com>
2026-09-04 09:42:41 +0300

    Add isolated security layout

---
+++ app/layouts/security.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen xl:flex">
+    <SecuritySidebar />
+    <div :class="['min-w-0 flex-1 transition-all duration-300 ease-in-out', isExpanded ? 'lg:ml-[290px]' : 'lg:ml-[90px]']">
+      <SecurityHeader />
+      <ImpersonationBanner />
+      <main class="mx-auto w-full max-w-[2000px] p-4 md:p-6"><slot /></main>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const { isExpanded } = useTailAdminSidebar()
+useTailAdminTheme()
+</script>
