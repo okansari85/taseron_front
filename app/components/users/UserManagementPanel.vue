06cc7940f2a0e4f3526ad33a37e78f3f1adb469d
OKAN SARI <sari_okan@hotmail.com>
2026-09-04 13:36:24 +0300

    Kullanıcı listesindeki aksiyon menüsünü kaldır

--- app/components/users/UserManagementPanel.vue
+++ app/components/users/UserManagementPanel.vue
@@ -3,7 +3,14 @@ import UserAuthorizationPanel from './UserAuthorizationPanel.vue'
 </script>
 
 <template>
-  <div class="space-y-3">
+  <div class="space-y-3 user-management-panel">
     <UserAuthorizationPanel />
   </div>
 </template>
+
+<style scoped>
+.user-management-panel :deep(thead th:last-child),
+.user-management-panel :deep(tbody td:last-child) {
+  display: none;
+}
+</style>
