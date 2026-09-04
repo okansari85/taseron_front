b1e2845a5b03363473e3134b214c01837799fbec
OKAN SARI <sari_okan@hotmail.com>
2026-09-04 17:09:43 +0300

    add role permissions page

---
+++ app/pages/tenants/[tenantId]/authorization/roles-permissions.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import RolePermissionsPanel from '~/components/users/RolePermissionsPanel.vue'
+</script>
+
+<template>
+  <div class="p-6">
+    <RolePermissionsPanel />
+  </div>
+</template>
