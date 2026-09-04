8db2ccdba809bda89ebadcb8ee09798b9d69f62f
OKAN SARI <sari_okan@hotmail.com>
2026-09-04 17:09:37 +0300

    add authorization users page

---
+++ app/pages/tenants/[tenantId]/authorization/users.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AuthorizationUsersPanel from '~/components/users/AuthorizationUsersPanel.vue'
+</script>
+
+<template>
+  <div class="p-6">
+    <AuthorizationUsersPanel />
+  </div>
+</template>
