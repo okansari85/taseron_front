250ea3bddd1e795412d9d44b8ad12fe832ad3584
OKAN SARI <sari_okan@hotmail.com>
2026-09-04 08:23:29 +0300

    Fix default layout template syntax and restore impersonation banner

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,9 +5,10 @@
       :class="[
         'min-w-0 flex-1 transition-all duration-300 ease-in-out',
         isExpanded ? 'lg:ml-[290px]' : 'lg:ml-[90px]',
-      ]
+      ]"
     >
       <TailAdminHeader />
+      <ImpersonationBanner />
       <main class="mx-auto w-full max-w-[2000px] p-4 md:p-6">
         <slot />
       </main>
