a90bb0b9b851b8af55e062834bbbec0a9e0883ed
OKAN SARI <sari_okan@hotmail.com>
2026-09-16 21:08:45 +0300

    fix: use desktop sidebar sizing consistently

--- app/layouts/isg-portal.vue
+++ app/layouts/isg-portal.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen xl:flex">
-    <IsgSidebar />
+    <IsgSidebar :desktop="true" />
     <div :class="['min-w-0 flex-1 transition-all duration-300 ease-in-out', isExpanded ? 'lg:ml-[240px]' : 'lg:ml-[72px]']">
       <IsgHeader />
       <ImpersonationBanner />
