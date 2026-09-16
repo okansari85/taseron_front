184e754cf578b26ba83acf26f3cb1c8da3d91e73
OKAN SARI <sari_okan@hotmail.com>
2026-09-16 21:08:24 +0300

    fix: align ISG content with sidebar

--- app/layouts/isg-portal.vue
+++ app/layouts/isg-portal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen xl:flex">
     <IsgSidebar />
-    <div :class="['min-w-0 flex-1 transition-all duration-300 ease-in-out', isExpanded ? 'lg:ml-[290px]' : 'lg:ml-[90px]']">
+    <div :class="['min-w-0 flex-1 transition-all duration-300 ease-in-out', isExpanded ? 'lg:ml-[240px]' : 'lg:ml-[72px]']">
       <IsgHeader />
       <ImpersonationBanner />
       <main class="mx-auto w-full max-w-[2000px] p-4 md:p-6"><slot /></main>
