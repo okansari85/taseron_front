c34f3ee17e594fb2d964c442f8cadc53188e59c6
okansari85 <sari_okan@hotmail.com>
2026-09-18 20:08:51 +0300

    asdsad

--- app/layouts/isg-portal.vue
+++ app/layouts/isg-portal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen xl:flex">
     <IsgSidebar :desktop="true" />
-    <div :class="['min-w-0 w-full flex-1 transition-all duration-300 ease-in-out', isExpanded ? 'lg:ml-[240px]' : 'lg:ml-[72px]']">
+    <div :class="['min-w-0 w-full flex-1 transition-all duration-300 ease-in-out', isExpanded ? 'lg:ml-[270px]' : 'lg:ml-[72px]']">
       <IsgHeader />
       <ImpersonationBanner />
       <main class="w-full p-4 md:p-6"><slot /></main>
